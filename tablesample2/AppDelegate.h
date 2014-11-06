d7fa193c474966b6920e8f7f2686d848951fe88d
smartshinobu <bizan07@bizankomumakku07-no-MacBook-Air-2.local>
2014-11-06 20:36:17 +0900

    Initial Commit

---
+++ tablesample2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tablesample2
+//
+//  Created by ビザンコムマック０７ on 2014/11/06.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tablesample2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tablesample2
+//
+//  Created by ビザンコムマック０７ on 2014/11/06.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
